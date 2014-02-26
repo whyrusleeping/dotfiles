fdf6e7e56f1ddd0b50482cb5c31529a9c9904b39
Jeromy <jeromyj@gmail.com>
2014-02-26 14:28:56 -0800

    update system setup scripts

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir -p ~/.config/fish
 ln ~/code/dotfiles/.vimrc ~/.vimrc
 ln -s ~/code/dotfiles/.vim ~/.vim
 ln -s ~/code/dotfiles/.fonts ~/.fonts
@@ -12,3 +13,16 @@ git submodule update
 
 #Load vim bundles
 vim +:BundleInstall +:qa
+
+#clone fish-shell down, make and install it
+cd ~/code && git clone https://github.com/fish-shell/fish-shell
+export CFLAGS="-O3"
+cd ~/code/fish-shell && autoconf && ./configure && \
+	make -j 16 && sudo make install
+
+echo "/usr/local/bin/fish" >> /etc/shells
+
+#clone awesome theme down
+cd ~/code && git clone https://github.com/whyrusleeping/powerarrow
+ln -s ~/code/powerarrow ~/.config/awesome
+
