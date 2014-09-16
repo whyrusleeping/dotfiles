ead739a8ecbb30507c5fd6c17ce472d3c82a8e3e
Jeromy <jeromyj@gmail.com>
2014-09-16 05:38:53 -0800

    update install script

--- install.sh
+++ install.sh
@@ -8,24 +8,3 @@ ln ~/code/dotfiles/.Xdefaults ~/.Xdefaults
 ln ~/code/dotfiles/config.fish ~/.config/fish/config.fish
 
 #urxvt clipboard scripts
-sudo cp clipboard /usr/lib/urxvt/perl/
-
-#initialize vundle
-git submodule init
-git submodule update
-
-#Load vim bundles
-vim +:BundleInstall +:qa
-
-#clone fish-shell down, make and install it
-cd ~/code && git clone https://github.com/fish-shell/fish-shell
-export CFLAGS="-O3"
-cd ~/code/fish-shell && autoconf && ./configure && \
-	make -j 16 && sudo make install
-
-echo "/usr/local/bin/fish" >> /etc/shells
-
-#clone awesome theme down
-cd ~/code && git clone https://github.com/whyrusleeping/powerarrow
-ln -s ~/code/powerarrow ~/.config/awesome
-
