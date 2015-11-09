e4c157f986f4753e26bbb2412f841ec2c6038865
Jeromy <jeromyj@gmail.com>
2015-11-09 11:27:38 -0800

    more packages

--- config.fish
+++ config.fish
@@ -2,3 +2,6 @@ set -x PATH $PATH /sbin /home/whyrusleeping/go/bin ~/gopkg/bin ~/bin
 set -x GOPATH /home/whyrusleeping/gopkg
 
 set -x EDITOR vim
+
+echo $SSH_AGENT_PID > $HOME/.ssh_agent_pid
+echo $SSH_AUTH_SOCK > $HOME/.ssh_auth_sock
--- scripts/packages-arch.sh
+++ scripts/packages-arch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo pacman -Syy
 sudo pacman -S xorg-server slim i3
-sudo pacman -S gcc gvim git clang rxvt-unicode ctags xorg-xmodmap xorg-xinit autoconf chromium nitrogen openssh htop conky xsel network-manager network-manager-applet gnome-keyring dmenu imagemagick 
+sudo pacman -S gcc gvim git clang rxvt-unicode ctags xorg-xmodmap xorg-xinit autoconf chromium nitrogen openssh htop conky xsel network-manager network-manager-applet gnome-keyring dmenu imagemagick unzip
 sudo pacman -S yubico-pam
 ./install-yaourt.sh
 yaourt -S byobu
